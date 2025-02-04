1db68c2220a1b6febc066047f750a35ed8b7db79
harmya <hvbhatt@purdue.edu>
2025-02-03 22:28:50 -0500

    guh

--- src/correction.rs
+++ src/correction.rs
@@ -20,7 +20,7 @@ pub fn encode_parity_bit(encoded_string: String) -> (bool, String) {
     }
 }
 
-pub fn decode_parity_bit(encoded_string: String) -> (bool, Vec<usize>, String) {
+pub fn decode_parity_bit(encoded_string: &String) -> (bool, bool, String) {
     let mut count_of_ones = 0;
 
     for char in encoded_string.chars() {
@@ -30,9 +30,9 @@ pub fn decode_parity_bit(encoded_string: String) -> (bool, Vec<usize>, String) {
     }
 
     if count_of_ones % 2 == 0 {
-        return (false, vec![], format!("No error in message"));
+        return (false, false, format!("No error in message"));
     } else {
-        return (false, vec![], format!("STOP! Found error in message"));
+        return (true, false, format!("STOP! Found error in message"));
     }
 }
 
@@ -52,7 +52,7 @@ pub fn encode_triple(encoded_string: String) -> (bool, String) {
     return (true, binary_string);
 }
 
-pub fn decode_triple(encoded_string: String) -> (bool, Vec<usize>, String) {
+pub fn decode_triple(encoded_string: &String) -> (bool, bool, String) {
     let first_nine_bits = &encoded_string[..9];
     let length_of_message = usize::from_str_radix(first_nine_bits, 2).unwrap();
 
@@ -66,7 +66,6 @@ pub fn decode_triple(encoded_string: String) -> (bool, Vec<usize>, String) {
         .collect();
 
     let mut num_errors: usize = 0;
-    let mut error_indices: Vec<usize> = vec![];
     for i in 0..length_of_message {
         let first_chunk_char = first_chunk[i];
         let second_chunk_char = second_chunk[i];
@@ -80,9 +79,8 @@ pub fn decode_triple(encoded_string: String) -> (bool, Vec<usize>, String) {
             + (second_chunk_char == '1') as u32
             + (third_chunk_char == '1') as u32;
 
-        if count_0 != 0 || count_1 != 0 {
+        if !(count_0 == 3 || count_1 == 3) {
             num_errors += 1;
-            error_indices.push(i);
         }
 
         first_chunk[i] = if count_1 >= count_0 { '1' } else { '0' };
@@ -90,7 +88,7 @@ pub fn decode_triple(encoded_string: String) -> (bool, Vec<usize>, String) {
 
     let final_string: String = first_chunk.into_iter().collect();
 
-    return (num_errors % 2 == 0, error_indices, final_string);
+    return (num_errors > 0, num_errors > 0, final_string);
 }
 
 pub fn encode_hamming(encoded_string: String, is_extended: bool) -> (bool, String) {
@@ -140,59 +138,67 @@ pub fn encode_hamming(encoded_string: String, is_extended: bool) -> (bool, Strin
     return (true, final_code);
 }
 
-pub fn decode_hamming(received_code: String, is_extended: bool) -> (bool, Vec<usize>, String) {
-    let total_length = received_code.len();
+pub fn decode_hamming(received_code: &String, is_extended: bool) -> (bool, bool, String) {
     let mut hamming_code: Vec<char> = received_code.chars().collect();
-
-    let overall_parity_bit = hamming_code.remove(0);
-    let received_overall_parity = overall_parity_bit == '0';
-
-    let computed_overall_parity = hamming_code.iter().filter(|&&c| c == '1').count() % 2 == 0;
-
+    let mut received_overall_parity: bool = false;
+    if is_extended {
+        let overall_parity_bit = hamming_code.remove(0);
+        received_overall_parity = overall_parity_bit == '0';
+    }
+    let total_length = hamming_code.len();
     let mut num_parity_bits = 0;
     while (1 << num_parity_bits) < total_length {
         num_parity_bits += 1;
     }
 
-    let mut error_positions = Vec::new();
     let mut curr_error_position: usize = 0;
-
+    let mut has_error = false;
     for i in 0..num_parity_bits {
         let parity_pos = 1 << i;
         let mut parity = 0;
 
-        for bit in (parity_pos..=total_length - 1).step_by(2 * parity_pos) {
+        for bit in (parity_pos..=total_length).step_by(2 * parity_pos) {
+            println!("bit {}", bit);
             for k in 0..parity_pos {
-                if bit + k - 1 < total_length - 1 {
+                if bit + k - 1 <= total_length - 1 {
                     parity ^= (hamming_code[bit + k - 1] as u8 - b'0') as i32;
                 }
             }
         }
 
+        println!("Parity found{}", parity);
+
         if parity % 2 != 0 {
+            println!("Parity pos{}", parity_pos);
             curr_error_position += parity_pos;
         }
+
+        println!();
     }
     let mut has_double_error = false;
-
+    println!("{}", curr_error_position);
     if curr_error_position > 0 {
-        if computed_overall_parity != received_overall_parity && is_extended {
-            has_double_error = true;
-        } else {
-            let idx = curr_error_position - 1;
-            error_positions.push(idx);
+        let idx = curr_error_position - 1;
+
+        if idx < hamming_code.len() {
+            has_error = true;
             hamming_code[idx] = if hamming_code[idx] == '0' { '1' } else { '0' };
+        }
+
+        let computed_overall_parity = hamming_code.iter().filter(|&&c| c == '1').count() % 2 == 0;
+        if is_extended && computed_overall_parity != received_overall_parity {
+            has_double_error = true;
         }
     }
 
     let mut original_data = String::new();
-    for i in 1..=total_length - 1 {
+    for i in 1..=total_length {
         if !i.is_power_of_two() {
             original_data.push(hamming_code[i - 1]);
         }
     }
 
-    (has_double_error, error_positions, original_data)
+    (has_error, (has_error && !has_double_error), original_data)
 }
 pub fn encode_correction(
     correction_type: CorrectionType,
@@ -207,12 +213,13 @@ pub fn encode_correction(
 
 pub fn decode_correction(
     correction_type: CorrectionType,
-    encoded_string: String,
-) -> (bool, Vec<usize>, String) {
+    encoded_string: &String,
+    is_extended: bool,
+) -> (bool, bool, String) {
     match correction_type {
-        CorrectionType::Parity => decode_parity_bit(encoded_string),
-        CorrectionType::Triple => decode_triple(encoded_string),
-        CorrectionType::Hamming => todo!(),
+        CorrectionType::Parity => decode_parity_bit(&encoded_string),
+        CorrectionType::Triple => decode_triple(&encoded_string),
+        CorrectionType::Hamming => decode_hamming(&encoded_string, is_extended),
     }
 }
 
@@ -237,15 +244,15 @@ mod tests {
     #[test]
     fn test_decode_parity_bit_no_error() {
         let input = String::from("01100");
-        let expected = (false, vec![], String::from("No error in message"));
-        assert_eq!(decode_parity_bit(input), expected);
+        let expected = (false, false, String::from("No error in message"));
+        assert_eq!(decode_parity_bit(&input), expected);
     }
 
     #[test]
     fn test_decode_parity_bit_with_error() {
         let input = String::from("111011");
-        let expected = (false, vec![], String::from("STOP! Found error in message"));
-        assert_eq!(decode_parity_bit(input), expected);
+        let expected = (true, false, String::from("STOP! Found error in message"));
+        assert_eq!(decode_parity_bit(&input), expected);
     }
 
     #[test]
@@ -258,15 +265,15 @@ mod tests {
     #[test]
     fn test_decode_triple_without_flip() {
         let input = String::from("000000110111101111101111101");
-        let expected = (true, vec![], String::from("111101"));
-        assert_eq!(decode_triple(input), expected);
+        let expected = (false, false, String::from("111101"));
+        assert_eq!(decode_triple(&input), expected);
     }
 
     #[test]
     fn test_decode_triple_with_one_flip() {
         let input = String::from("000000110111101111101111100");
-        let expected = (true, vec![5], String::from("111101"));
-        assert_eq!(decode_triple(input), expected);
+        let expected = (true, true, String::from("111101"));
+        assert_eq!(decode_triple(&input), expected);
     }
 
     #[test]
@@ -279,21 +286,31 @@ mod tests {
     #[test]
     fn test_decode_hamming_one() {
         let input = String::from("101011011");
-        let expected = (false, vec![], String::from("11101"));
-        assert_eq!(decode_correction(CorrectionType::Hamming, input), expected);
+        let expected = (false, false, String::from("11101"));
+        println!("{:?}", expected);
+        assert_eq!(
+            decode_correction(CorrectionType::Hamming, &input, false),
+            expected
+        );
     }
 
     #[test]
     fn test_decode_hamming_with_single_bit_error() {
         let input = String::from("101111011");
-        let expected = (true, vec![3], String::from("11101"));
-        assert_eq!(decode_correction(CorrectionType::Hamming, input), expected);
+        let expected = (true, true, String::from("11101"));
+        assert_eq!(
+            decode_correction(CorrectionType::Hamming, &input, false),
+            expected
+        );
     }
 
     #[test]
     fn test_decode_hamming_with_double_bit_error() {
-        let input = String::from("101111111"); // Errors introduced
-        let expected = (false, vec![], String::from("11101"));
-        assert_eq!(decode_correction(CorrectionType::Hamming, input), expected);
+        let input = String::from("0101111111");
+        let expected = (true, false, String::from("01111"));
+        assert_eq!(
+            decode_correction(CorrectionType::Hamming, &input, true),
+            expected
+        );
     }
 }
